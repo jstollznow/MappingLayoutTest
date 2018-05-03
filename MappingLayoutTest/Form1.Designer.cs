12c2dd8a88230b26c72a36e4494e507d4ef23cf3
Jacob <jacob.stollznow@fyidocs.com>
2018-05-03 13:51:21 +0930

    first commit

--- MappingLayoutTest/Form1.Designer.cs
+++ MappingLayoutTest/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MappingLayoutTest
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MapGridView = new System.Windows.Forms.DataGridView();
+            this.order = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mapDestination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MapLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.map = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.MapGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MapGridView
+            // 
+            this.MapGridView.AllowUserToResizeRows = false;
+            this.MapGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MapGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.MapGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MapGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.order,
+            this.mapDestination,
+            this.MapLocation,
+            this.map});
+            this.MapGridView.Location = new System.Drawing.Point(12, 45);
+            this.MapGridView.MultiSelect = false;
+            this.MapGridView.Name = "MapGridView";
+            this.MapGridView.ReadOnly = true;
+            this.MapGridView.RowHeadersVisible = false;
+            this.MapGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.MapGridView.Size = new System.Drawing.Size(677, 393);
+            this.MapGridView.TabIndex = 0;
+            // 
+            // order
+            // 
+            this.order.FillWeight = 10F;
+            this.order.HeaderText = "Order: ";
+            this.order.Name = "order";
+            this.order.ReadOnly = true;
+            // 
+            // mapDestination
+            // 
+            this.mapDestination.FillWeight = 42.5F;
+            this.mapDestination.HeaderText = "Map To: ";
+            this.mapDestination.Name = "mapDestination";
+            this.mapDestination.ReadOnly = true;
+            this.mapDestination.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // MapLocation
+            // 
+            this.MapLocation.FillWeight = 42.5F;
+            this.MapLocation.HeaderText = "Map From:";
+            this.MapLocation.Name = "MapLocation";
+            this.MapLocation.ReadOnly = true;
+            this.MapLocation.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // map
+            // 
+            this.map.FillWeight = 5F;
+            this.map.HeaderText = "";
+            this.map.Name = "map";
+            this.map.ReadOnly = true;
+            this.map.TrackVisitedState = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(701, 450);
+            this.Controls.Add(this.MapGridView);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MapGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView MapGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mapDestination;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MapLocation;
+        private System.Windows.Forms.DataGridViewLinkColumn map;
     }
 }
 
--- MappingLayoutTest/Form1.cs
+++ MappingLayoutTest/Form1.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace MappingLayoutTest
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MapGridView.DefaultCellStyle.SelectionBackColor = MapGridView.DefaultCellStyle.BackColor;
+            MapGridView.DefaultCellStyle.SelectionForeColor = MapGridView.DefaultCellStyle.ForeColor;
+            DataGridViewRow example = (DataGridViewRow)MapGridView.Rows[0].Clone();
+            example.Cells[0].Value = "1";
+            example.Cells[1].Value = "Clients";
+            example.Cells[2].Value = "People";
+            example.Cells[3].Value = "Map";
+            MapGridView.Rows.Add(example);
+            DataGridViewRow example1 = (DataGridViewRow)MapGridView.Rows[0].Clone();
+            example1.Cells[0].Value = "2";
+            example1.Cells[1].Value = "Cabinets";
+            example1.Cells[2].Value = "Files";
+            example1.Cells[3].Value = "Map";
+            MapGridView.Rows.Add(example1);
         }
     }
 }
