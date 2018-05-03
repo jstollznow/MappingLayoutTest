3dc6ef0e1ebca8dfef0556ce8094a3530f0dbf61
Jacob <jacob.stollznow@fyidocs.com>
2018-05-03 15:36:46 +0930

    layout good

--- MappingLayoutTest/Form1.Designer.cs
+++ MappingLayoutTest/Form1.Designer.cs
@@ -29,10 +29,14 @@
         private void InitializeComponent()
         {
             this.MapGridView = new System.Windows.Forms.DataGridView();
+            this.DirectoryLabel = new System.Windows.Forms.Label();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.DirectoryTextbox = new System.Windows.Forms.TextBox();
             this.order = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mapDestination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FYIField = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.MapLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.map = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.IsMapped = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.MapGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,17 +50,49 @@
             this.MapGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.MapGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.order,
-            this.mapDestination,
+            this.FYIField,
             this.MapLocation,
-            this.map});
+            this.map,
+            this.IsMapped});
+            this.MapGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.MapGridView.Location = new System.Drawing.Point(12, 45);
             this.MapGridView.MultiSelect = false;
             this.MapGridView.Name = "MapGridView";
-            this.MapGridView.ReadOnly = true;
             this.MapGridView.RowHeadersVisible = false;
             this.MapGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.MapGridView.Size = new System.Drawing.Size(677, 393);
             this.MapGridView.TabIndex = 0;
+            this.MapGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MapGridView_CellContentClick_1);
+            this.MapGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.MapGridView_CellContentClick);
+            // 
+            // DirectoryLabel
+            // 
+            this.DirectoryLabel.AutoSize = true;
+            this.DirectoryLabel.Location = new System.Drawing.Point(17, 15);
+            this.DirectoryLabel.Name = "DirectoryLabel";
+            this.DirectoryLabel.Size = new System.Drawing.Size(92, 13);
+            this.DirectoryLabel.TabIndex = 5;
+            this.DirectoryLabel.Text = "Current Directory: ";
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(614, 10);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.TabIndex = 4;
+            this.BrowseButton.Text = "Browse...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // DirectoryTextbox
+            // 
+            this.DirectoryTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DirectoryTextbox.Location = new System.Drawing.Point(115, 12);
+            this.DirectoryTextbox.Name = "DirectoryTextbox";
+            this.DirectoryTextbox.Size = new System.Drawing.Size(493, 20);
+            this.DirectoryTextbox.TabIndex = 3;
             // 
             // order
             // 
@@ -65,13 +101,11 @@
             this.order.Name = "order";
             this.order.ReadOnly = true;
             // 
-            // mapDestination
+            // FYIField
             // 
-            this.mapDestination.FillWeight = 42.5F;
-            this.mapDestination.HeaderText = "Map To: ";
-            this.mapDestination.Name = "mapDestination";
-            this.mapDestination.ReadOnly = true;
-            this.mapDestination.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.FYIField.FillWeight = 42.5F;
+            this.FYIField.HeaderText = "FYI Destination";
+            this.FYIField.Name = "FYIField";
             // 
             // MapLocation
             // 
@@ -83,33 +117,48 @@
             // 
             // map
             // 
-            this.map.FillWeight = 5F;
+            this.map.FillWeight = 10F;
             this.map.HeaderText = "";
+            this.map.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
             this.map.Name = "map";
-            this.map.ReadOnly = true;
             this.map.TrackVisitedState = false;
+            // 
+            // IsMapped
+            // 
+            this.IsMapped.FillWeight = 10F;
+            this.IsMapped.HeaderText = "Mapped?";
+            this.IsMapped.Name = "IsMapped";
+            this.IsMapped.ReadOnly = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(701, 450);
+            this.Controls.Add(this.DirectoryLabel);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.DirectoryTextbox);
             this.Controls.Add(this.MapGridView);
             this.Name = "MainForm";
             this.Text = "Main Form";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MapGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView MapGridView;
+        private System.Windows.Forms.Label DirectoryLabel;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.TextBox DirectoryTextbox;
         private System.Windows.Forms.DataGridViewTextBoxColumn order;
-        private System.Windows.Forms.DataGridViewTextBoxColumn mapDestination;
+        private System.Windows.Forms.DataGridViewComboBoxColumn FYIField;
         private System.Windows.Forms.DataGridViewTextBoxColumn MapLocation;
         private System.Windows.Forms.DataGridViewLinkColumn map;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IsMapped;
     }
 }
 
--- MappingLayoutTest/Form1.cs
+++ MappingLayoutTest/Form1.cs
@@ -21,18 +21,70 @@ namespace MappingLayoutTest
         {
             MapGridView.DefaultCellStyle.SelectionBackColor = MapGridView.DefaultCellStyle.BackColor;
             MapGridView.DefaultCellStyle.SelectionForeColor = MapGridView.DefaultCellStyle.ForeColor;
+
+            ComboBox FYImapping = new ComboBox();
+            FYImapping.Items.Add("Client");
+            FYImapping.Items.Add("Cabinet");
+            FYImapping.Items.Add("Project");
+            FYImapping.Items.Add("Work Type");
+
+            ((DataGridViewComboBoxColumn)MapGridView.Columns[1]).DataSource = FYImapping.Items;
+
             DataGridViewRow example = (DataGridViewRow)MapGridView.Rows[0].Clone();
             example.Cells[0].Value = "1";
-            example.Cells[1].Value = "Clients";
-            example.Cells[2].Value = "People";
+            //example.Cells[1].Value = "Clients";
+            example.Cells[2].Value = "";
             example.Cells[3].Value = "Map";
+            example.Cells[4].Value= char.ConvertFromUtf32(0x00002716);
             MapGridView.Rows.Add(example);
             DataGridViewRow example1 = (DataGridViewRow)MapGridView.Rows[0].Clone();
             example1.Cells[0].Value = "2";
-            example1.Cells[1].Value = "Cabinets";
-            example1.Cells[2].Value = "Files";
+            //example1.Cells[1].Value = "Cabinets";
+            example1.Cells[2].Value = "";
             example1.Cells[3].Value = "Map";
+            example1.Cells[4].Value = char.ConvertFromUtf32(0x00002714);
             MapGridView.Rows.Add(example1);
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            //select directory
+            FolderBrowserDialog SelectDirectory = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.UserProfile,
+                Description = "Please select the folder containing the documents to upload: ",
+                ShowNewFolderButton = false
+            };
+
+            if (SelectDirectory.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryTextbox.Text = SelectDirectory.SelectedPath;
+            }
+        }
+
+        private void MapGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if(senderGrid.Columns[e.ColumnIndex] is DataGridViewLinkColumn && e.ColumnIndex==3)
+            {
+                FolderBrowserDialog SelectDirectory = new FolderBrowserDialog
+                {
+                    RootFolder = Environment.SpecialFolder.UserProfile,
+                    Description = "Please select the folder containing the documents to upload: ",
+                    ShowNewFolderButton = false
+                };
+
+                if (SelectDirectory.ShowDialog() == DialogResult.OK)
+                {
+                    senderGrid.Rows[e.RowIndex].Cells[2].Value = SelectDirectory.SelectedPath;
+                }
+            }
+            
+        }
+
+        private void MapGridView_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+           
         }
     }
 }
